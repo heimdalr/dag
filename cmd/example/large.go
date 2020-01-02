d4a415426ec2b3d5a4151778fd1b8519f1318f7d
Sebastian Bogan <sebogh@qibli.net>
2020-01-02 20:12:55 +0100

    added timing example

---
+++ cmd/example/large.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"github.com/heimdalr/dag"
+	"math"
+	"time"
+)
+
+type largeVertex struct {
+	value int
+}
+
+func (v largeVertex) String() string {
+	return fmt.Sprintf("%d", v.value)
+}
+
+func main() {
+	d := dag.NewDAG()
+	root := &largeVertex{1}
+	levels := 7
+	branches := 9
+	var start, end time.Time
+
+	start = time.Now()
+	largeAux(d, levels, branches, root)
+	end = time.Now()
+	fmt.Printf("%fs to add edges\n", end.Sub(start).Seconds())
+	expectedVertexCount := sum(0, levels - 1 , branches, pow)
+	vertexCount := len(d.GetVertices())
+	if vertexCount != expectedVertexCount {
+		panic(fmt.Sprintf("GetVertices() = %d, want %d", vertexCount, expectedVertexCount))
+	}
+
+	start = time.Now()
+	descendants, _ := d.GetDescendants(root)
+	end = time.Now()
+	fmt.Printf("%fs to get descendants\n", end.Sub(start).Seconds())
+	descendantsCount := len(descendants)
+	expectedDescendantsCount := vertexCount - 1
+	if descendantsCount != expectedDescendantsCount {
+		panic(fmt.Sprintf("GetDescendants(root) = %d, want %d", descendantsCount, expectedDescendantsCount))
+	}
+
+	start = time.Now()
+	descendants, _ = d.GetDescendants(root)
+	end = time.Now()
+	fmt.Printf("%fs to get descendants 2nd time\n", end.Sub(start).Seconds())
+
+	start = time.Now()
+	children, _ := d.GetChildren(root)
+	end = time.Now()
+	fmt.Printf("%fs to get children\n", end.Sub(start).Seconds())
+	childrenCount := len(children)
+	expectedChildrenCount := branches
+	if childrenCount != expectedChildrenCount {
+		panic(fmt.Sprintf("GetChildren(root) = %d, want %d", childrenCount, expectedChildrenCount))
+	}
+
+	var childList []dag.Vertex
+	for x := range children {
+		childList = append(childList, x)
+	}
+	start = time.Now()
+	_ = d.DeleteEdge(root, childList[0])
+	end = time.Now()
+	fmt.Printf("%fs to delete an edge from the root\n", end.Sub(start).Seconds())
+
+}
+
+func largeAux(d *dag.DAG, level int, branches int, parent *largeVertex) {
+	if level > 1 {
+		if branches < 1 || branches > 9 {
+			panic("number of branches must be between 1 and 9")
+		}
+		for i := 1; i <= branches; i++ {
+			value := (*parent).value*10 + i
+			child := &largeVertex{value}
+			err := d.AddEdge(parent, child)
+			if err != nil {
+				panic(err)
+			}
+			largeAux(d, level-1, branches, child)
+		}
+	}
+}
+
+func sum(x, y, branches int, fn interface{}) int {
+	if x > y {
+		return 0
+	}
+	f, ok := fn.(func(int, int) int)
+	if !ok {
+		panic("function no of correct tpye")
+	}
+	current := f(branches, x)
+	rest := sum(x+1, y, branches, f)
+	return current + rest
+}
+
+func pow(base int, exp int) int {
+	pow := math.Pow(float64(base), float64(exp))
+	return int(pow)
+}
+
