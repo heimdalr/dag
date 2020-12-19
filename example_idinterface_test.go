73c6c71f8317c8cbe9426e64fd9918e5399cae02
sebogh@qibli.net <sebogh@qibli.net>
2020-12-19 17:56:30 +0100

    example for IDInterface

---
+++ example_idinterface_test.go
@@ -0,0 +1,30 @@
+package dag_test
+
+import (
+	"fmt"
+	"github.com/heimdalr/dag"
+)
+type idVertex struct {
+	id  string
+	msg string
+}
+
+func (v idVertex) ID() string {
+	return v.id
+}
+
+func ExampleIDInterface() {
+
+	// initialize a new graph
+	d := dag.NewDAG()
+
+	// init three vertices
+	id, _ := d.AddVertex(idVertex{id: "1", msg: "foo"})
+	fmt.Printf("id of vertex is %s\n", id)
+	v, _ := d.GetVertex(id)
+	fmt.Printf("%s", v)
+
+	// Output:
+	// id of vertex is 1
+	// {1 foo}
+}
