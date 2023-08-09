b78424bb480cd429cce8ccddf52517c0069dd840
Christian Kniep <christian@qnib.org>
2023-08-09 10:21:53 +0200

    add GetOutboundEdges

--- dag.go
+++ dag.go
@@ -1309,3 +1309,7 @@ func (d *dMutex) unlock(i interface{}) {
 	// release the global lock
 	d.globalMutex.Unlock()
 }
+
+func (d *DAG) GetOutboundEdges() map[interface{}]map[interface{}]struct{} {
+	return d.outboundEdge
+}
